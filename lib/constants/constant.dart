3fa86afe9d855d82f6048ab7f2d8f59fd3da754d
Hamamul Fauzi <hamamul.fauzi@gmail.com>
2022-05-10 16:19:56 +0700

    ui height

--- lib/constants/constant.dart
+++ lib/constants/constant.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 Color primaryColor = const Color(0xff0a0e21);
-TextStyle? genderTextStyle =  TextStyle(
-  fontSize: 18,
-  color: Color(0xff8d8e98),
-);
+TextStyle? labelTextStyle =
+    TextStyle(fontSize: 18, color: Colors.white //Color(0xff8d8e98),
+        );
