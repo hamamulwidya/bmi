e6cc4bc19ba73f9024baa21e4ef31f3b8c806f13
Hamamul Fauzi <hamamul.fauzi@gmail.com>
2022-05-10 19:17:15 +0700

    ui halaman hasil

--- lib/views/bmi_result_screen.dart
+++ lib/views/bmi_result_screen.dart
@@ -1,16 +1,87 @@
-
+import 'package:bmi/views/bmi_data_screen.dart';
 import 'package:flutter/material.dart';
 
 class BmiResultScreen extends StatelessWidget {
-  const BmiResultScreen({ Key? key }) : super(key: key);
+  const BmiResultScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Hasil Hitung BMI"),
-      ),
-      
-    );
+        appBar: AppBar(
+          title: Text("Hasil Hitung BMI"),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                child: Center(
+                  child: Text(
+                    "Hasil Perhitungan",
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: BmiCard(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        "Normal",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        "18.5",
+                        style: TextStyle(
+                          fontSize: 100,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        "Your BMI result is quite low, you should eat more",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Container(
+                height: 80,
+                color: Color(0xffEC3C66),
+                child: Center(
+                  child: Text(
+                    "Hitung Ulang",
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
