4542f664ccfb3c63723cadac85a214c26b2af0c7
Hamamul Fauzi <hamamul.fauzi@gmail.com>
2022-05-10 12:10:40 +0700

    car gender

--- lib/bmi_data_screen.dart
+++ lib/bmi_data_screen.dart
@@ -1,14 +1,14 @@
 import 'package:bmi/bmi_result_screen.dart';
 import 'package:flutter/material.dart';
 
-class BmiDataScree extends StatefulWidget {
-  const BmiDataScree({Key? key}) : super(key: key);
+class BmiDataScreen extends StatefulWidget {
+  const BmiDataScreen({Key? key}) : super(key: key);
 
   @override
-  State<BmiDataScree> createState() => _BmiDataScreeState();
+  State<BmiDataScreen> createState() => _BmiDataScreenState();
 }
 
-class _BmiDataScreeState extends State<BmiDataScree> {
+class _BmiDataScreenState extends State<BmiDataScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,9 +18,56 @@ class _BmiDataScreeState extends State<BmiDataScree> {
       body: Column(
         children: [
           Expanded(
-              child: Container(
-            color: Colors.red,
-          )),
+            child: Container(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(
+                            Icons.male,
+                            size: 80,
+                          ),
+                          Text(
+                            "Male",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Color(0xff8d8e98),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          Icons.female,
+                          size: 80,
+                        ),
+                        Text(
+                          "Female",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color(0xff8d8e98),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
           Expanded(
               child: Container(
             color: Colors.yellow,
